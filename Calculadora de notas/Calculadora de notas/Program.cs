e938554547d70c938f8d306ed1c84d076df2954e
juandavid-dotcom <davidrriddikc@gmail.com>
2026-06-01 22:22:50 -0400

    Finished

---
+++ Calculadora de notas/Calculadora de notas/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+decimal nota;
+
+Console.WriteLine("Calculadora de notas");
+Console.Write("Ingrese una calificacion ");
+
+try
+{
+    nota = Convert.ToDecimal(Console.ReadLine());
+
+    if (nota < 0 || nota > 100)
+    {
+        Console.WriteLine("La nota debe estar entre 0 y 100.");
+    }
+    else if (nota >= 90)
+    {
+        Console.WriteLine("Calificaci0n : A");
+    }
+    else if (nota >= 80)
+    {
+        Console.WriteLine("Calificacion : B");
+    }
+    else if (nota >= 70)
+    {
+        Console.WriteLine("Calificacion: C ");
+    }
+    else if (nota >= 60)
+    {
+        Console.WriteLine("Calificacion: D ");
+    }
+    else
+    {
+        Console.WriteLine("Calificacion : F");
+    }
+}
+catch
+{
+    Console.WriteLine("Debe ingresar un número válido.");
+}
+
+Console.ReadKey();
+
+
+
+
+
+
+
---
+++ Conversor de Temperatura/Conversor de Temperatura/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+
+
+
+
+
+decimal temperatura;
+int opcion;
+
+Console.WriteLine("CONVERSOR DE TEMPERATURA");
+Console.WriteLine("1. Celsius a Fahrenheit");
+Console.WriteLine("2. Fahrenheit a Celsius");
+Console.Write("Seleccione una opcion: ");
+
+try
+{
+    opcion = Convert.ToInt32(Console.ReadLine());
+
+
+
+    Console.Write("Ingrese la temperatura: ");
+    temperatura = Convert.ToDecimal(Console.ReadLine());
+
+
+
+    if (opcion == 1)
+    {
+        decimal fahrenheit = (temperatura * 9 / 5) + 32;
+        Console.WriteLine("Resultado: " + fahrenheit + " °F");
+    }
+    else if (opcion == 2)
+    {
+        decimal celsius = (temperatura - 32) * 5 / 9;
+        Console.WriteLine("Resultado: " + celsius + " °C");
+    }
+    else
+    {
+        Console.WriteLine("opcion no valida.");
+    }
+
+
+
+}
+catch
+{
+    Console.WriteLine(" debe ingresar numeros validos");
+}
+
+Console.ReadKey();
