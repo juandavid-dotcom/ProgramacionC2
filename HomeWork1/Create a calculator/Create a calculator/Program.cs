0cf724f7b20b80457dd77128d4e979fcb1229a64
juandavid-dotcom <davidrriddikc@gmail.com>
2026-06-09 22:26:28 -0400

    Testing the  calculator

--- HomeWork1/Create a calculator/Create a calculator/Program.cs
+++ HomeWork1/Create a calculator/Create a calculator/Program.cs
@@ -20,35 +20,87 @@ while (option != 6)
         switch (option)
         {
             case 1:
-                Console.WriteLine("Opcion Suma");
-                break;
 
             case 2:
-                Console.WriteLine("Opcion Resta");
-                break;
 
             case 3:
-                Console.WriteLine("Opcion Multiplicacion");
-                break;
 
             case 4:
-                Console.WriteLine("Opcion Dividicion");
+
+              decimal[]numbers = new decimal[2];
+
+              Console.WriteLine(" Escribe el primer numero");
+              numbers[0] = Convert.ToDecimal(Console.ReadLine());
+
+              Console.WriteLine(" Escribe el segundo nuemro");
+              numbers[1]= Convert.ToDecimal(Console.ReadLine());
+
+              decimal result = 0;
+
+                 switch(option)
+                {
+                    case 1:
+                        result=numbers[0] + numbers[1];
+                        Console.WriteLine(" Total:" + result);
+                        break;
+
+                    case 2:
+                        result=numbers[0] - numbers[1];
+                        Console.WriteLine(" Total:" + result);
+                        break;
+
+                    case 3:
+                        result=numbers[0] * numbers[1];
+                        Console.WriteLine(" Total:" + result);
+                        break;
+
+                    case 4:
+                        if(numbers[1] == 0)
+                        {
+                            Console.WriteLine(" Recuerda que no se puede dividir entre cero!");
+                        }
+                        else
+                        {
+                            result=numbers[0] / numbers[1];
+                            Console.WriteLine(" Total:" + result);
+                        }
+                        break;
+                }
+
                 break;
 
-            case 5:
-                Console.WriteLine("Opcion Evaluar al Estudiante");
-                break;
+                /*  switch (option)
+                  {
+                      case 1:
+                          Console.WriteLine("Opcion Suma");
+                          break;
 
-            case 6:
-                Console.WriteLine("Programa finalizado.");
-                break;
+                      case 2:
+                          Console.WriteLine("Opcion Resta");
+                          break;
 
-            default:
-                Console.WriteLine("Opcion no valida.");
-                break;
+                      case 3:
+                          Console.WriteLine("Opcion Multiplicacion");
+                          break;
+
+                      case 4:
+                          Console.WriteLine("Opcion Dividicion");
+                          break;
+
+                      case 5:
+                          Console.WriteLine("Opcion Evaluar al Estudiante");
+                          break;
+
+                      case 6:
+                          Console.WriteLine("Programa finalizado.");
+                          break;
+
+                      default:
+                          Console.WriteLine("Opcion no valida.");
+                          break; */
         }
 
-        
+
     }
     catch
     {
